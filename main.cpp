46a9f2039986a7ca416ab26ac661211b79fc84d8
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2015-03-18 00:08:30 +0300

    Pano compositing extracted as separate function.

--- main.cpp
+++ main.cpp
@@ -72,6 +72,12 @@ int blend_type = Blender::MULTI_BAND;
 float blend_strength = 5;
 string result_name = "result.jpg";
 
+float find_features_time = 0;
+float partwise_matching_time = 0;
+float warping_time = 0;
+float compositing_time = 0;
+float total_time = 0;
+
 void printUsage();
 int parseCmdArgs(int argc, char** argv);
 
@@ -94,14 +100,164 @@ void findFeatures(const vector<Mat>& full_imgs, vector<ImageFeatures>& features,
     finder.collectGarbage();
 }
 
+Mat composePano(const vector<Mat>& full_imgs, vector<CameraParams>& cameras, double work_scale, float warped_image_scale)
+{
+    double seam_scale = min(1.0, sqrt(seam_megapix * 1e6 / full_imgs[0].size().area()));
+    double seam_work_aspect = seam_scale / work_scale;
+    double compose_work_aspect = 1. / work_scale;
+
+    Mat img_warped, img_warped_s;
+    Mat dilated_mask, seam_mask, mask, mask_warped;
+    Ptr<Blender> blender;
+
+    vector<Mat> images(full_imgs.size());
+    vector<Point> corners(full_imgs.size());
+    vector<Mat> masks_warped(full_imgs.size());
+    vector<Mat> images_warped(full_imgs.size());
+    vector<Size> sizes(full_imgs.size());
+    vector<Mat> masks(full_imgs.size());
+
+    cout << "Downscaling for futher processing..." << endl;
+    for (size_t i = 0; i < full_imgs.size(); ++i)
+        resize(full_imgs[i], images[i], Size(), seam_scale, seam_scale);
+
+    // Preapre images masks
+    for (size_t i = 0; i < full_imgs.size(); ++i)
+    {
+        masks[i].create(images[i].size(), CV_8U);
+        masks[i].setTo(Scalar::all(255));
+    }
+
+    // Warp images and their masks
+
+    cout << "Warping images (auxiliary)..." << endl;
+    double t = getTickCount();
+    Ptr<WarperCreator> warper_creator;
+#if defined(HAVE_OPENCV_GPU)
+    if (try_gpu && gpu::getCudaEnabledDeviceCount() > 0)
+        warper_creator = new cv::SphericalWarperGpu();
+    else
+#endif
+        warper_creator = new cv::SphericalWarper();
+
+    Ptr<RotationWarper> warper = warper_creator->create(
+                static_cast<float>(warped_image_scale * seam_work_aspect));
+
+    for (size_t i = 0; i < full_imgs.size(); ++i)
+    {
+        Mat_<float> K;
+        cameras[i].K().convertTo(K, CV_32F);
+        float swa = (float)seam_work_aspect;
+        K(0,0) *= swa; K(0,2) *= swa; K(1,1) *= swa; K(1,2) *= swa;
+
+        corners[i] = warper->warp(images[i], K, cameras[i].R, INTER_LINEAR,
+                                  BORDER_REFLECT, images_warped[i]);
+        sizes[i] = images_warped[i].size();
+
+        warper->warp(masks[i], K, cameras[i].R, INTER_NEAREST, BORDER_CONSTANT, masks_warped[i]);
+    }
+
+    vector<Mat> images_warped_f(full_imgs.size());
+    for (size_t i = 0; i < images_warped.size(); ++i)
+        images_warped[i].convertTo(images_warped_f[i], CV_32F);
+
+    warping_time = (getTickCount() - t) / getTickFrequency();
+
+    cout << "Exposure compensation..." << endl;
+    Ptr<ExposureCompensator> compensator = ExposureCompensator::createDefault(expos_comp_type);
+    compensator->feed(corners, images_warped, masks_warped);
+
+    Ptr<SeamFinder> seam_finder;
+#if defined(HAVE_OPENCV_GPU)
+    if (try_gpu && gpu::getCudaEnabledDeviceCount() > 0)
+        seam_finder = new detail::GraphCutSeamFinderGpu(GraphCutSeamFinderBase::COST_COLOR);
+    else
+#endif
+        seam_finder = new detail::GraphCutSeamFinder(GraphCutSeamFinderBase::COST_COLOR);
+
+    seam_finder->find(images_warped_f, corners, masks_warped);
+
+    // Release unused memory
+    images.clear();
+    images_warped.clear();
+    images_warped_f.clear();
+    masks.clear();
+
+    cout << "Compositing..." << endl;
+    t = getTickCount();
+
+    // Update warped image scale
+    warped_image_scale *= static_cast<float>(compose_work_aspect);
+    warper = warper_creator->create(warped_image_scale);
+
+    // Update corners and sizes
+    for (size_t i = 0; i < cameras.size(); ++i)
+    {
+        // Update intrinsics
+        cameras[i].focal *= compose_work_aspect;
+        cameras[i].ppx *= compose_work_aspect;
+        cameras[i].ppy *= compose_work_aspect;
+
+        Mat K;
+        cameras[i].K().convertTo(K, CV_32F);
+        Rect roi = warper->warpRoi(full_imgs[i].size(), K, cameras[i].R);
+        corners[i] = roi.tl();
+        sizes[i] = roi.size();
+    }
+
+    for (size_t img_idx = 0; img_idx < full_imgs.size(); ++img_idx)
+    {
+        cout << "Compositing image #" << img_idx << endl;
+
+        Mat K;
+        cameras[img_idx].K().convertTo(K, CV_32F);
+
+        // Warp the current image
+        warper->warp(full_imgs[img_idx], K, cameras[img_idx].R, INTER_LINEAR, BORDER_REFLECT, img_warped);
+
+        // Warp the current image mask
+        mask.create(full_imgs[img_idx].size(), CV_8U);
+        mask.setTo(Scalar::all(255));
+        warper->warp(mask, K, cameras[img_idx].R, INTER_NEAREST, BORDER_CONSTANT, mask_warped);
+
+        // Compensate exposure
+        compensator->apply(img_idx, corners[img_idx], img_warped, mask_warped);
+
+        img_warped.convertTo(img_warped_s, CV_16S);
+        img_warped.release();
+        mask.release();
+
+        dilate(masks_warped[img_idx], dilated_mask, Mat());
+        resize(dilated_mask, seam_mask, mask_warped.size());
+        mask_warped = seam_mask & mask_warped;
+
+        if (blender.empty())
+        {
+            blender = Blender::createDefault(blend_type, try_gpu);
+            Size dst_sz = resultRoi(corners, sizes).size();
+            float blend_width = sqrt(static_cast<float>(dst_sz.area())) * blend_strength / 100.f;
+            if (blend_width < 1.f)
+                blender = Blender::createDefault(Blender::NO, try_gpu);
+            else
+            {
+                MultiBandBlender* mb = dynamic_cast<MultiBandBlender*>(static_cast<Blender*>(blender));
+                mb->setNumBands(static_cast<int>(ceil(log(blend_width)/log(2.)) - 1.));
+            }
+            blender->prepare(corners, sizes);
+        }
+
+        // Blend the current image
+        blender->feed(img_warped_s, mask_warped, corners[img_idx]);
+    }
+
+    Mat result, result_mask;
+    blender->blend(result, result_mask);
+
+    return result;
+}
+
 int main(int argc, char* argv[])
 {
-    float find_features_time = 0;
-    float partwise_matching_time = 0;
-    float warping_time = 0;
-    float compositing_time = 0;
-    float total_time = 0;
-
     cv::setBreakOnError(true);
 
     int retval = parseCmdArgs(argc, argv);
@@ -117,13 +273,10 @@ int main(int argc, char* argv[])
     }
 
     vector<Mat> full_imgs(num_images);
-    vector<Size> full_img_sizes(num_images);
-    vector<Mat> images(num_images);
 
     for (size_t i = 0; i < num_images; ++i)
     {
         full_imgs[i] = imread(img_names[i]);
-        full_img_sizes[i] = full_imgs[i].size();
 
         if (full_imgs[i].empty())
         {
@@ -135,20 +288,13 @@ int main(int argc, char* argv[])
     cout << "Files reading finished" << endl;
 
     int64 app_start_time = getTickCount();
-    double work_scale = min(1.0, sqrt(work_megapix * 1e6 / full_img_sizes[0].area()));
-    double seam_scale = min(1.0, sqrt(seam_megapix * 1e6 / full_img_sizes[0].area()));
-    double seam_work_aspect = seam_scale / work_scale;
-    double compose_work_aspect = 1. / work_scale;
+    double work_scale = min(1.0, sqrt(work_megapix * 1e6 / full_imgs[0].size().area()));
 
     cout << "Finding features..." << endl;
     vector<ImageFeatures> features;
     int64 t = getTickCount();
     findFeatures(full_imgs, features, work_scale);
     find_features_time = (getTickCount() - t) / getTickFrequency();
-
-    cout << "Downscaling for futher processing..." << endl;
-    for (size_t i = 0; i < num_images; ++i)
-        resize(full_imgs[i], images[i], Size(), seam_scale, seam_scale);
 
     cout << "Pairwise matching..." << endl;
     t = getTickCount();
@@ -161,22 +307,16 @@ int main(int argc, char* argv[])
     // Leave only images we are sure are from the same panorama
     cout << "Finding biggest connected component..." << endl;
     vector<int> indices = leaveBiggestComponent(features, pairwise_matches, conf_thresh);
-    vector<Mat> img_subset;
     vector<string> img_names_subset;
     vector<Mat> full_imgs_subset;
-    vector<Size> full_img_sizes_subset;
     for (size_t i = 0; i < indices.size(); ++i)
     {
         img_names_subset.push_back(img_names[indices[i]]);
-        img_subset.push_back(images[indices[i]]);
         full_imgs_subset.push_back(full_imgs[indices[i]]);
-        full_img_sizes_subset.push_back(full_img_sizes[indices[i]]);
     }
 
-    images = img_subset;
     img_names = img_names_subset;
     full_imgs = full_imgs_subset;
-    full_img_sizes = full_img_sizes_subset;
 
     // Check if we still have enough images
     num_images = img_names.size();
@@ -226,149 +366,8 @@ int main(int argc, char* argv[])
     for (size_t i = 0; i < cameras.size(); ++i)
         cameras[i].R = rmats[i];
 
-    cout << "Warping images (auxiliary)..." << endl;
     t = getTickCount();
-
-    vector<Point> corners(num_images);
-    vector<Mat> masks_warped(num_images);
-    vector<Mat> images_warped(num_images);
-    vector<Size> sizes(num_images);
-    vector<Mat> masks(num_images);
-
-    // Preapre images masks
-    for (size_t i = 0; i < num_images; ++i)
-    {
-        masks[i].create(images[i].size(), CV_8U);
-        masks[i].setTo(Scalar::all(255));
-    }
-
-    // Warp images and their masks
-
-    Ptr<WarperCreator> warper_creator;
-#if defined(HAVE_OPENCV_GPU)
-    if (try_gpu && gpu::getCudaEnabledDeviceCount() > 0)
-        warper_creator = new cv::SphericalWarperGpu();
-    else
-#endif
-        warper_creator = new cv::SphericalWarper();
-
-    Ptr<RotationWarper> warper = warper_creator->create(
-                static_cast<float>(warped_image_scale * seam_work_aspect));
-
-    for (size_t i = 0; i < num_images; ++i)
-    {
-        Mat_<float> K;
-        cameras[i].K().convertTo(K, CV_32F);
-        float swa = (float)seam_work_aspect;
-        K(0,0) *= swa; K(0,2) *= swa; K(1,1) *= swa; K(1,2) *= swa;
-
-        corners[i] = warper->warp(images[i], K, cameras[i].R, INTER_LINEAR,
-                                  BORDER_REFLECT, images_warped[i]);
-        sizes[i] = images_warped[i].size();
-
-        warper->warp(masks[i], K, cameras[i].R, INTER_NEAREST, BORDER_CONSTANT, masks_warped[i]);
-    }
-
-    vector<Mat> images_warped_f(num_images);
-    for (size_t i = 0; i < num_images; ++i)
-        images_warped[i].convertTo(images_warped_f[i], CV_32F);
-
-    warping_time = (getTickCount() - t) / getTickFrequency();
-
-    cout << "Exposure compensation..." << endl;
-    Ptr<ExposureCompensator> compensator = ExposureCompensator::createDefault(expos_comp_type);
-    compensator->feed(corners, images_warped, masks_warped);
-
-    Ptr<SeamFinder> seam_finder;
-#if defined(HAVE_OPENCV_GPU)
-    if (try_gpu && gpu::getCudaEnabledDeviceCount() > 0)
-        seam_finder = new detail::GraphCutSeamFinderGpu(GraphCutSeamFinderBase::COST_COLOR);
-    else
-#endif
-        seam_finder = new detail::GraphCutSeamFinder(GraphCutSeamFinderBase::COST_COLOR);
-
-    seam_finder->find(images_warped_f, corners, masks_warped);
-
-    // Release unused memory
-    images.clear();
-    images_warped.clear();
-    images_warped_f.clear();
-    masks.clear();
-
-    cout << "Compositing..." << endl;
-    t = getTickCount();
-
-    Mat img_warped, img_warped_s;
-    Mat dilated_mask, seam_mask, mask, mask_warped;
-    Ptr<Blender> blender;
-
-    // Update warped image scale
-    warped_image_scale *= static_cast<float>(compose_work_aspect);
-    warper = warper_creator->create(warped_image_scale);
-
-    // Update corners and sizes
-    for (size_t i = 0; i < num_images; ++i)
-    {
-        // Update intrinsics
-        cameras[i].focal *= compose_work_aspect;
-        cameras[i].ppx *= compose_work_aspect;
-        cameras[i].ppy *= compose_work_aspect;
-
-        Mat K;
-        cameras[i].K().convertTo(K, CV_32F);
-        Rect roi = warper->warpRoi(full_img_sizes[i], K, cameras[i].R);
-        corners[i] = roi.tl();
-        sizes[i] = roi.size();
-    }
-
-    for (size_t img_idx = 0; img_idx < num_images; ++img_idx)
-    {
-        cout << "Compositing image #" << indices[img_idx]+1 << endl;
-
-        Mat K;
-        cameras[img_idx].K().convertTo(K, CV_32F);
-
-        // Warp the current image
-        warper->warp(full_imgs[img_idx], K, cameras[img_idx].R, INTER_LINEAR, BORDER_REFLECT, img_warped);
-
-        // Warp the current image mask
-        mask.create(full_imgs[img_idx].size(), CV_8U);
-        mask.setTo(Scalar::all(255));
-        warper->warp(mask, K, cameras[img_idx].R, INTER_NEAREST, BORDER_CONSTANT, mask_warped);
-
-        // Compensate exposure
-        compensator->apply(img_idx, corners[img_idx], img_warped, mask_warped);
-
-        img_warped.convertTo(img_warped_s, CV_16S);
-        img_warped.release();
-        mask.release();
-
-        dilate(masks_warped[img_idx], dilated_mask, Mat());
-        resize(dilated_mask, seam_mask, mask_warped.size());
-        mask_warped = seam_mask & mask_warped;
-
-        if (blender.empty())
-        {
-            blender = Blender::createDefault(blend_type, try_gpu);
-            Size dst_sz = resultRoi(corners, sizes).size();
-            float blend_width = sqrt(static_cast<float>(dst_sz.area())) * blend_strength / 100.f;
-            if (blend_width < 1.f)
-                blender = Blender::createDefault(Blender::NO, try_gpu);
-            else
-            {
-                MultiBandBlender* mb = dynamic_cast<MultiBandBlender*>(static_cast<Blender*>(blender));
-                mb->setNumBands(static_cast<int>(ceil(log(blend_width)/log(2.)) - 1.));
-            }
-            blender->prepare(corners, sizes);
-        }
-
-        // Blend the current image
-        blender->feed(img_warped_s, mask_warped, corners[img_idx]);
-    }
-
-    Mat result, result_mask;
-    blender->blend(result, result_mask);
-
+    Mat result = composePano(full_imgs, cameras, work_scale, warped_image_scale);
     compositing_time = (getTickCount() - t) / getTickFrequency();
 
     total_time = (getTickCount() - app_start_time) / getTickFrequency();
