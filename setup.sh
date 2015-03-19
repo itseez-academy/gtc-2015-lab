011c458e22f161f1c5ae5d30a632b7f072730955
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2015-03-19 22:57:36 +0300

    Sample file structure reorganized.

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get install -y g++ make cmake
-wget http://developer.download.nvidia.com/embedded/OpenCV/L4T_21.2/libopencv4tegra-repo_l4t-r21_2.4.10.1_armhf.deb
-sudo dpkg -i libopencv4tegra-repo_l4t-r21_2.4.10.1_armhf.deb
-sudo apt-get update
-sudo apt-get install -y libopencv4tegra libopencv4tegra-dev
