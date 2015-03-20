042d1c55cc3ac1dd050270cb0e54e1df0493989f
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2015-03-20 16:25:48 +0300

    Power management script adopted for r21.3

--- freqScale_l4t.sh
+++ freqScale_l4t.sh
@@ -37,7 +37,7 @@ echo 1 > /sys/devices/system/cpu/cpu1/online
 echo 1 > /sys/devices/system/cpu/cpu2/online
 echo 1 > /sys/devices/system/cpu/cpu3/online
 echo userspace > /sys/devices/system/cpu/cpuquiet/current_governor
-echo 0 > /sys/kernel/debug/edp/vdd_cpu/edp_reg_override
+echo 0 > /sys/kernel/debug/edp/vdd_cpu/cpu_edp_reg_override
 echo "userspace" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo "userspace" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo "userspace" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
